cc71c61317a9788e0cc7acdaf4db073b58af7fc9
JerinBoyd <maxers@mac.com>
2018-03-14 18:37:08 -0400

    created a product router and wired up the router to the main server app

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-const express = require('express');
+const express = require("express");
 const serverApp = express();
 
-const PORT = process.env.PORT || 5000;  // necc for heroku deployment
+const PORT = process.env.PORT || 5000; // necc for heroku deployment
 
-serverApp.get('/', (req, res) => {
-    res.send('hi');
-})
+//routers
+const productRouter = require('./routers/products');
+
+serverApp.use(productRouter); //register the router with the application
+
+serverApp.get("/", (req, res) => {
+  res.send("yes it still works");
+});
 
 serverApp.listen(PORT, () => {
-    console.log(`Now listening on port $(PORT)`);
+  console.log(`Now listening on port $(PORT)`);
 });
