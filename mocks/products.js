4a9adbf4bf517651920afe504abff78937913859
JerinBoyd <maxers@mac.com>
2018-03-14 19:30:09 -0400

    created mock product data and served that as a get requested and response

---
+++ mocks/products.js
@@ -0,0 +1,31 @@
+const products = [
+    {
+        _id:  'asidhfoi',
+        name: 'Javelin (caution)',
+        price: 500,
+        imgSrc: 'https://via.placeholder.com/250x250',
+        created: new Date(),
+
+    },
+    {
+        _id:  'asasderoi',
+        name: 'Snail',
+        price: 3.99,
+        imgSrc: 'https://via.placeholder.com/250x250',
+        created: new Date(),
+
+    },
+    {
+        _id:  'asiadsi',
+        name: 'Rug',
+        price: 99.99,
+        imgSrc: 'https://via.placeholder.com/250x250',
+        created: new Date(),
+
+    },
+];
+
+
+
+
+module.exports = products;
