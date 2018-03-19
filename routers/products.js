0d7bd8be4a8970578d5575cce180a13360445427
JerinBoyd <maxers@mac.com>
2018-03-19 18:21:57 -0400

    added delete and update messages to products/router.js

--- routers/products.js
+++ routers/products.js
@@ -3,10 +3,11 @@ const router = express.Router();
 const mockProducts = require("../mocks/products");
 
 const Product = require("../models/product");
-const productArrayToObject = arrayOfProducts => {
-  //create an accumaltor object
+
+const productArrToObj = arrayOfProducts => {
+  //create an accumulator object
   const accumulator = {};
-  // for each product in arrayOfProducts
+  //for Each product in arrayOfProducts
   arrayOfProducts.forEach(product => {
     const id = product._id;
     const copy = { ...product._doc };
@@ -14,9 +15,9 @@ const productArrayToObject = arrayOfProducts => {
     accumulator[id] = copy;
   });
   //grab the id
-  //delete the underscore id internal to the object
-  //set the if value in the accumalator object equal to product
-  //return accumalator
+  //delete the _id internal to the object
+  //set the id value in the accumulator object equal to product
+  //return accumulator
   return accumulator;
 };
 
@@ -25,12 +26,12 @@ router.get("/products", (req, res) => {
     .exec()
     .then(allProducts => {
       res.status(200).json({
-        products: productArrayToObject(allProducts)
+        products: productArrToObj(allProducts)
       });
     })
     .catch(err => {
-      re.status(500).json({
-        msg: "broken again"
+      res.status(500).json({
+        msg: "Brokked again!!!"
       });
     });
 });
@@ -40,9 +41,9 @@ router.get("/products/:id", (req, res) => {
   Product.findById(id)
     .exec()
     .then(selectedProduct => {
-        const selectedId = selectedProduct._id;
-        const copy = {...selectedProduct.doc};
-        delete copy._id
+      const selectedId = selectedProduct._id;
+      const copy = { ...selectedProduct._doc };
+      delete copy._id;
       res.status(200).json({
         products: {
           [selectedId]: copy
@@ -51,29 +52,39 @@ router.get("/products/:id", (req, res) => {
     })
     .catch(err => {
       res.status(500).json({
-        masg: "really really broke"
+        msg: "do not look behind you."
       });
     });
 });
 
+//post means create
 router.post("/products", (req, res) => {
   const product = new Product({
     name: "something new",
     price: 1000,
     imgSrc: "https://via.placeholder.com/250x250"
   });
-  product.save().then(response => {
-    res
-      .status(201)
-      .json({
-        msg: "successsfully created product"
-      })
-      .catch(err => {
-        res.status(500).json({
-          msg: "You stuf is broked"
-        });
+  product
+    .save()
+    .then(response => {
+      res.status(201).json({
+        msg: "successfully created product"
       });
-  });
+    })
+    .catch(err => {
+      res.status(500).json({
+        msg: "Your stuff done broke."
+      });
+    });
 });
 
-module.exports = router; // like export default
+//update (put)
+router.put("/products/:id", (req, res) => {
+  res.send("Updating Now");
+});
+// delete
+router.delete("/products/:id", (req, res) => {
+  res.send("deleting something right now");
+});
+
+module.exports = router; //like export default
