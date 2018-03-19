97a1f73ec45c1641f1275345ad6aa5d8464b3c34
JerinBoyd <maxers@mac.com>
2018-03-19 19:24:52 -0400

    created simple logger middlerware

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 const express = require("express");
 const serverApp = express();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-require('dotenv').config();
+require("dotenv").config();
 
-mongoose.connect(process.env.MONGO_URI); //has to be put after require('dotenv')
-const PORT = process.env.PORT || 5000; // necc for heroku deployment
+mongoose.connect(process.env.MONGO_URI);
+const PORT = process.env.PORT || 5000; //necessary for Heroku deployment
 
 //routers
-const productRouter = require('./routers/products');
+const productRouter = require("./routers/products");
+serverApp.use(function logger(req, res, next) {
+  const { url } = req;
+  const date = new Date();
+  console.log(`URL: ${url} @ ${date}`);
+  next();
+});
 
 serverApp.use(productRouter); //register the router with the application
 
 serverApp.get("/", (req, res) => {
-  res.send("yes it still works");
+  res.send("Something better");
 });
 
 serverApp.listen(PORT, () => {
-  console.log(`Now listening on port $(PORT)`);
+  console.log(`Now listening on port ${PORT}`);
 });
